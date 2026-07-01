2ad8fa72b3b942deb331e629e0467537ccba33e2
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-02 00:41:17 +0530

    Create ForLoop.py

---
+++ revise_practice/ForLoop.py
@@ -0,0 +1,42 @@
+i=1
+for i in range(5):
+  print("Hi")
+  i +=1
+
+for i in range(5):
+  print(i+1)
+
+for i in range (10,0,-1):
+  print(i)
+
+for i in range(10):
+  print(10-i)
+
+for i in range(0,11,2):
+  print(i)
+
+for i in range(6):
+  print(i*2)
+
+for i in range(1,12,2):
+  print(i)
+
+for i in "Devanshu":
+  print(i, end=" ")
+
+for i in range(2,8,1):
+  print(i*i)
+
+for i in range(1,11,1):
+  print(i*5)
+
+for i in range(1,11,1):
+  print(5*i)
+
+for i in range(1,50,1):
+  if(5%i==0):
+    print(i)
+
+for i in range(1,50,1):
+  if(i%5==0):
+    print(i)
