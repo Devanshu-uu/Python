08ca8b206c9cf0f9b6257c82c7c8f2a40cfd4582
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-07 21:46:53 +0530

    Create password.py

---
+++ loops/While Loop/password.py
@@ -0,0 +1,13 @@
+password="hello"
+user=""
+
+while(user!=password):
+    
+    user=input("Enter password: ")
+    if(user!=password):
+        print("Incorrect Password")
+
+print("Correct Password")
+
+
+    
