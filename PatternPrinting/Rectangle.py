68bc18221aecab893ecc757db6f18aa258d6655d
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-07-09 03:06:54 +0530

    Add pattern printing examples
    Add two new pattern printing scripts: 12345.py for number patterns and Rectangle.py for asterisk patterns. Also update continueQue.py to print 'Done' upon completion.

---
+++ PatternPrinting/Rectangle.py
@@ -0,0 +1,8 @@
+# for i in range(4):
+#     print("* "*5)
+
+
+for row in range(4):
+    for col in range(5):
+        print("*",end=" ")
+    print()
--- break&continue/continueQue.py
+++ break&continue/continueQue.py
@@ -2,4 +2,4 @@ for i in range(1,11):
     if (i%2!=0):
         continue
     print(i)
-        
+print("Done") 
