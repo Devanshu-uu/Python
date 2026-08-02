7eeda0cd2ea3d0e1b37251d893437e747639557c
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-02 23:08:37 +0530

    Update Problem5.py

--- Problems/Problem5.py
+++ Problems/Problem5.py
@@ -10,9 +10,20 @@ for i in range(5):
 print(sum(price))
 
 
-maxi=price.index(max(price))
-mini=price.index(min(price))
-print(item[maxi])
-print(item[mini])
+# maxi=price.index(max(price))
+# mini=price.index(min(price))
+# print(item[maxi])
+# print(item[mini])
 
-
+mx=price[0]
+mx_item=item[0]
+mn=price[0]
+mn_item=item[0]
+for i in range(5):
+    if price[i]> mx:
+        mx_item=item[i]
+        mx=price[i]
+    elif price[i]< mn:
+        mn_item=item[i]
+        mn=price[i]
+print(f'{mx_item,mx} and {mn_item,mn}')
