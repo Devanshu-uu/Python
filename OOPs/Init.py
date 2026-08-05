c10cdbf74de29e4ba6a97505e028901c9882a175
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-05 21:22:44 +0530

    Add basic OOP examples (Person, ClassName)
    Add two new example scripts under OOPs: Init.py defines a Person class with an __init__ constructor and prints two instances (Raj, Devanshu). class&Object.py demonstrates a ClassName with a class attribute x=10 and prints x from three instances. Small, self-contained examples for learning constructors and class vs instance attribute access.

---
+++ OOPs/Init.py
@@ -0,0 +1,11 @@
+class Person:
+
+    def __init__(self,name ,city):
+        self.name=name
+        self.city=city
+
+p1=Person("Raj" , "Delhi")  
+print(p1.name, p1.city) 
+
+p2=Person("Devanshu" , "Delhi")  
+print(p2.name, p2.city)    
---
+++ OOPs/class&Object.py
@@ -0,0 +1,8 @@
+class ClassName:
+    x=10
+p1= ClassName()
+print(p1.x)
+p2= ClassName()
+print(p2.x)
+p3= ClassName()
+print(p3.x)
