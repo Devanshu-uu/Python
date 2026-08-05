d62e9277db839f04d2f06102bdf3ebf2b3d813dc
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-05 20:33:47 +0530

    Create Problem8.py

---
+++ Problems/Problem8.py
@@ -0,0 +1,6 @@
+student_first={"english","maths","hindi"}
+student_second={"english","maths","history"}
+
+print(student_first & student_second)
+print(student_first - student_second)
+print(student_second - student_first)
