90237777990b7725c418b72f733b401eda734745
Devanshu <141940273+Devanshu-uu@users.noreply.github.com>
2026-08-09 21:16:26 +0530

    badic

---
+++ tryy.py
@@ -0,0 +1,10 @@
+students= [{'roll': 101, 'name': 'Rahul', 'age': 20, 'course': 'Python', 'marks': 85},
+{'roll': 102, 'name': 'Neha', 'age': 19,  'course':'SQL', 'marks': 92},
+{'roll': 103, 'name': 'Amit', 'age': 22, 'course': 'Python', 'marks': 74},
+{'roll': 104, 'name': 'Priya', 'age': 21, 'course': 'AI', 'marks': 96} ,
+{'roll': 105, 'name': 'Rohan', 'age': 20, 'course': 'Python', 'marks': 67}]
+
+
+
+for student in students:
+    print(student["name"])
