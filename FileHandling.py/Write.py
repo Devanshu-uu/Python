afde57b519005811eb5ee0277db05c60e201c472
Devanshu <devanshumohriya@gmail.com>
2026-08-13 22:04:44 +0530

    append

--- FileHandling.py/Write.py
+++ FileHandling.py/Write.py
@@ -3,3 +3,14 @@ file.write("Suraj\n")
 file.write("Shiva\n")
 file.write("Devanshu\n")
 file.close()
+
+file=open("sample.txt","r")
+data=file.readlines()
+print(data)
+file.close()
+
+file= open("sample.txt","a")
+
+file.write("Gulshan")
+
+file.close()
