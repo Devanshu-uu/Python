ee0ddab5cf4a3806df5a06ad0b45db2a33b1f029
Devanshu <devanshumohriya@gmail.com>
2026-08-13 19:51:13 +0530

    Update list.py

--- FuctionalPrograming/list.py
+++ FuctionalPrograming/list.py
@@ -1,11 +1,12 @@
 list=[1,2,3,4]
 
-def list_sq(list ):
-    list2=[]
-    for elem in list:
-        list2.append(elem**2)
-    print(list2)
+
+list2=[]
+for elem in list:
+    list2.append(elem**2)
+    
+print(list2)
 
 
-list_sq(list)
+
 
